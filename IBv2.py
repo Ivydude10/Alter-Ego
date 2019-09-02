f286eeff87c7a4eeee2a7eb3df3747f898c26b37
Ivydude10 <54464061+Ivydude10@users.noreply.github.com>
2019-09-02 21:43:58 +1000

    Update IBv2.py

--- IBv2.py
+++ IBv2.py
@@ -31,152 +31,6 @@ async def hello(ctx):
     msg = 'Hello <@{}>. Welcome to the server! ~~Prepare to die.~~'.format(ctx.message.author.id)
     await ctx.send(msg)
 
-@client.command()
-async def caesar(ctx, text, s):
-    result = ""
-   # transverse the plain text
-    for i in range(len(text)):
-      char = text[i]
-      # Encrypt uppercase characters in plain text
-
-      if (char.isupper()):
-         result += chr((ord(char) + int(s)-65) % 26 + 65)
-      # Encrypt lowercase characters in plain text
-      elif (char.isspace()):
-          result += ' '
-      else:
-         result += chr((ord(char) + int(s) - 97) % 26 + 97)
-    embeded = discord.Embed(
-        title='Caesar Encrypted',
-        colour=discord.Colour.default()
-    )
-    embeded.add_field(name="Plain Text:", value=text, inline=False)
-    embeded.add_field(name="Shift Number:", value=s, inline=False)
-    embeded.add_field(name="Cipher:", value=result, inline=False)
-    await ctx.send(embed=embeded)
-
-@client.command()
-async def caesarbf(ctx, text):
-    embeded = discord.Embed(
-        title='Caesar Decrypted',
-        colour=discord.Colour.default()
-    )
-    embeded.add_field(name="Plain Text:", value=text, inline=False)
-    for i in range(25):
-        result = ""
-        for j in range(len(text)):
-            char = text[j]
-            if (char.isupper()):
-                result+= chr((ord(char) - i-65) % 26 + 65)
-            elif (char.isspace()):
-                result += ' '
-            else:
-                result += chr((ord(char) - i - 97) % 26 + 97)
-        embeded.add_field(name="Shift #" + str(i), value=result, inline=False)
-    await ctx.send(embed=embeded)
-
-
-@client.command(name='htt',
-                description="Converts hexadecimal code into text.",
-                brief="Hex to Text")
-async def hex_to_text(ctx, *hex_data):
-    ascii_string = str(base64.b16decode(''.join(hex_data), casefold=True))[2:-1]
-    embeded = discord.Embed(
-        title='Hex Decrypted',
-        colour=discord.Colour.default()
-    )
-    embeded.add_field(name="Decrypted Text:", value=ascii_string, inline=False)
-    await ctx.send(embed=embeded)
-
-@client.command(name='tth',
-                description="Converts text into hexadecimal code.",
-                brief="Text to Hex")
-async def text_to_hex(ctx,  *ascii_string):
-   ascii_string2 = str.encode(' '.join(ascii_string))
-   hex_data = str(base64.b16encode(ascii_string2))[2:-1]
-   embeded = discord.Embed(
-        title='Hex Encrypted',
-        colour=discord.Colour.default()
-   )
-   embeded.add_field(name="Encrypted Text:", value=hex_data, inline=False)
-   await ctx.send(embed=embeded)
-
-@client.command(name='b64tt',
-                description="Converts base 64 into text.",
-                brief="Base64 to Text")
-async def base_to_text(ctx, base_data):
-     ascii_string = str(base64.b64decode(base_data))[2:-1]
-     embeded = discord.Embed(
-          title='Base64 Decrypted',
-          colour=discord.Colour.default()
-     )
-     embeded.add_field(name="Decrypted Text:", value=ascii_string, inline=False)
-     await ctx.send(embed=embeded)
-
-@client.command(name='ttb64',
-                description="Converts text into base64",
-                brief="Text to Base64")
-async def text_to_base(ctx, *ascii_string):
-   ascii_string2 = str.encode(' '.join(ascii_string))
-   base_data = str(base64.b64encode(ascii_string2))[2:-1]
-   embeded = discord.Embed(
-        title='Base64 Encrypted',
-        colour=discord.Colour.default()
-   )
-   embeded.add_field(name="Encrypted Text:", value=base_data, inline=False)
-   await ctx.send(embed=embeded)
-
-@client.command(name='b32tt',
-                description="Converts base 32 into text.",
-                brief="Base32 to Text")
-async def base_to_text(ctx, base_data):
-    ascii_string = str(base64.b32decode(base_data))[2:-1]
-    embeded = discord.Embed(
-         title='Base32 Decrypted',
-         colour=discord.Colour.default()
-    )
-    embeded.add_field(name="Decrypted Text:", value=ascii_string, inline=False)
-    await ctx.send(embed=embeded)
-
-@client.command(name='ttb32',
-                description="Converts text into base32",
-                brief="Text to Base32")
-async def text_to_base(ctx, *ascii_string):
-   ascii_string2 = str.encode(' '.join(ascii_string))
-   base_data = str(base64.b32encode(ascii_string2))[2:-1]
-   embeded = discord.Embed(
-        title='Base32 Encrypted',
-        colour=discord.Colour.default()
-   )
-   embeded.add_field(name="Encrypted Text:", value=base_data, inline=False)
-   await ctx.send(embed=embeded)
-
-@client.command(name='bintext',
-                description="Converts binary code into text",
-                brief='Binary to Text'
-)
-async def binary_to_text(ctx, s):
-    output = ''.join(chr(int(s[i*8:i*8+8],2)) for i in range(len(s)//8))
-    embeded = discord.Embed(
-         title='Binary Decrypted',
-         colour=discord.Colour.default()
-    )
-    embeded.add_field(name="Decrypted Text:", value=output, inline=False)
-    await ctx.send(embed=embeded)
-
-@client.command(name='textbin',
-                description="Converts text into binary code",
-                brief='Text to Binary'
-)
-async def text_to_binary(ctx, *text):
-    output = ''.join([bin(ord(c))[2:].rjust(8, '0') for c in """ """.join(text)])
-    embeded = discord.Embed(
-         title='Binary Encrypted',
-         colour=discord.Colour.default()
-    )
-    embeded.add_field(name="Encrypted Text:", value=output, inline=False)
-    await ctx.send(embed=embeded)
-
 @client.command(pass_context=True)
 async def praise(ctx, *ascii_string):
     string = ' '.join(ascii_string)
@@ -771,6 +625,9 @@ async def removerole(ctx, rolename=""):
 #            print(err)
 #        else:
 #            print("Payload delivered successfully, code {}.".format(result.status_code))
-for filename in os.listdir('./cogs')
+
+for filename in os.listdir('./cogs'):
+    if filename.endswith('.py'):
+        client.load_extension(f'cogs.{filename[:-3]}')
 
 client.run('NTM2OTA3MTMxNDcxNzkwMDg3.Dyd0VQ.BED0UqPiQffV0ig7kdVy3BoykfM')
