85dad71d851c3e63d75d64f9b141f25ab02db5b9
Ivydude10 <54464061+Ivydude10@users.noreply.github.com>
2019-09-02 21:49:19 +1000

    Update ARG.py

--- cogs/ARG.py
+++ cogs/ARG.py
@@ -44,7 +44,7 @@ class ARG(commands.Cog):
         await ctx.send(embed=embeded)
 
     @commands.command()
-    async def caesarbf(ctx, text):
+    async def caesarbf(self, ctx, text):
         embeded = discord.Embed(
             title='Caesar Decrypted',
             colour=discord.Colour.default()
@@ -66,7 +66,7 @@ class ARG(commands.Cog):
     @commands.command(name='htt',
                     description="Converts hexadecimal code into text.",
                     brief="Hex to Text")
-    async def hex_to_text(ctx, *hex_data):
+    async def hex_to_text(self, ctx, *hex_data):
         ascii_string = str(base64.b16decode(''.join(hex_data), casefold=True))[2:-1]
         embeded = discord.Embed(
             title='Hex Decrypted',
@@ -78,7 +78,7 @@ class ARG(commands.Cog):
     @commands.command(name='tth',
                     description="Converts text into hexadecimal code.",
                     brief="Text to Hex")
-    async def text_to_hex(ctx,  *ascii_string):
+    async def text_to_hex(self, ctx,  *ascii_string):
        ascii_string2 = str.encode(' '.join(ascii_string))
        hex_data = str(base64.b16encode(ascii_string2))[2:-1]
        embeded = discord.Embed(
@@ -91,7 +91,7 @@ class ARG(commands.Cog):
     @commands.command(name='b64tt',
                     description="Converts base 64 into text.",
                     brief="Base64 to Text")
-    async def base_to_text(ctx, base_data):
+    async def base_to_text(self, ctx, base_data):
          ascii_string = str(base64.b64decode(base_data))[2:-1]
          embeded = discord.Embed(
               title='Base64 Decrypted',
@@ -103,7 +103,7 @@ class ARG(commands.Cog):
     @commands.command(name='ttb64',
                     description="Converts text into base64",
                     brief="Text to Base64")
-    async def text_to_base(ctx, *ascii_string):
+    async def text_to_base(self, ctx, *ascii_string):
        ascii_string2 = str.encode(' '.join(ascii_string))
        base_data = str(base64.b64encode(ascii_string2))[2:-1]
        embeded = discord.Embed(
@@ -116,7 +116,7 @@ class ARG(commands.Cog):
     @commands.command(name='b32tt',
                     description="Converts base 32 into text.",
                     brief="Base32 to Text")
-    async def base_to_text(ctx, base_data):
+    async def base_to_text(self, ctx, base_data):
         ascii_string = str(base64.b32decode(base_data))[2:-1]
         embeded = discord.Embed(
              title='Base32 Decrypted',
@@ -128,7 +128,7 @@ class ARG(commands.Cog):
     @commands.command(name='ttb32',
                     description="Converts text into base32",
                     brief="Text to Base32")
-    async def text_to_base(ctx, *ascii_string):
+    async def text_to_base(self, ctx, *ascii_string):
        ascii_string2 = str.encode(' '.join(ascii_string))
        base_data = str(base64.b32encode(ascii_string2))[2:-1]
        embeded = discord.Embed(
@@ -142,7 +142,7 @@ class ARG(commands.Cog):
                     description="Converts binary code into text",
                     brief='Binary to Text'
     )
-    async def binary_to_text(ctx, s):
+    async def binary_to_text(self, ctx, s):
         output = ''.join(chr(int(s[i*8:i*8+8],2)) for i in range(len(s)//8))
         embeded = discord.Embed(
              title='Binary Decrypted',
@@ -155,7 +155,7 @@ class ARG(commands.Cog):
                     description="Converts text into binary code",
                     brief='Text to Binary'
     )
-    async def text_to_binary(ctx, *text):
+    async def text_to_binary(self, ctx, *text):
         output = ''.join([bin(ord(c))[2:].rjust(8, '0') for c in """ """.join(text)])
         embeded = discord.Embed(
              title='Binary Encrypted',
