2daa5ac6bff5ebc3b7e3624570aa54c99e2c6c0f
Ivydude10 <54464061+Ivydude10@users.noreply.github.com>
2019-09-03 11:40:44 +1000

    Update Music.py

--- cogs/Music.py
+++ cogs/Music.py
@@ -10,7 +10,7 @@ from async_timeout import timeout
 from functools import partial
 from youtube_dl import YoutubeDL
 
-discord.opus.load_opus()
+discord.opus.load_opus('opus')
 
 ytdlopts = {
     'format': 'bestaudio/best',
