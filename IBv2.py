6d65b90bd5d5f3c932906cc9a3979213227e33d7
Ivydude10 <54464061+Ivydude10@users.noreply.github.com>
2019-09-17 07:39:59 +1000

    Update IBv2.py

--- IBv2.py
+++ IBv2.py
@@ -14,7 +14,7 @@ from discord import Game
 from discord.ext import commands
 from discord.ext.commands import Bot
 
-BOT_PREFIX = ("~", "&", "ivybot")
+BOT_PREFIX = ("~", "&", "ivybot ", "ivy bot ")
 
 client = Bot(command_prefix=BOT_PREFIX)
 
