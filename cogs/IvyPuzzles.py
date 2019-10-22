c157d7038919636ee5628fe29a8ed76baec50fb3
Ivydude10 <54464061+Ivydude10@users.noreply.github.com>
2019-10-22 13:09:42 +1100

    Update IvyPuzzles.py

--- cogs/IvyPuzzles.py
+++ cogs/IvyPuzzles.py
@@ -36,20 +36,20 @@ class IvyPuzzles(commands.Cog):
             resp = "Invalid Number"
             message = """
             So, you tried to be clever huh?
-        Thought you could gain early access to IP4?
-        Well too bad, since it isnt available yet.
-        However, I'll give you a little something for trying this out....
-        6 links have been created for IP4.
-        5 are just for fun, whereas one of them is a hint towards a secret.
-        I'm not gonna tell you which is which, and the secret will not be active until the puzzle is released.
-        So, have fun searching!! :)
+    Thought you could gain early access to IP4?
+    Well too bad, since it isnt available yet.
+    However, I'll give you a little something for trying this out....
+    6 links have been created for IP4.
+    5 are just for fun, whereas one of them is a hint towards a secret.
+    I'm not gonna tell you which is which, and the secret will not be active until the puzzle is released.
+    So, have fun searching!! :)
 
 
 
-        What? You want a hint of some kind?
-        Ugh, fine. You might wanna revist the links of the previous puzzles... ;)
+    What? You want a hint of some kind?
+    Ugh, fine. You might wanna revist the links of the previous puzzles... ;)
 
-        P.S. You might wanna keep this to yourself. Only one person can claim the secret."""
+    P.S. You might wanna keep this to yourself. Only one person can claim the secret."""
             await ctx.message.author.send(message)
         else:
             resp = "Invalid Number"
