d35d04de079b074311b1e4fc95daeccc4dceddbf
Ivydude10 <54464061+Ivydude10@users.noreply.github.com>
2019-10-27 16:37:59 +1100

    Update IBv2.py

--- IBv2.py
+++ IBv2.py
@@ -53,7 +53,7 @@ async def unload(ctx, extension):
 #            print("Payload delivered successfully, code {}.".format(result.status_code))
 @client.event
 async def on_ready():
-    await client.change_presence(activity=Game(name="with the idea of Despair"))
+    await client.change_presence(activity=Game(name="with the idea of Hope"))
     print("Logged in as " + client.user.name)
 
 for filename in os.listdir('./cogs'):
