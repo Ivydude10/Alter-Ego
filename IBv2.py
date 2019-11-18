7a1c42cc56b22f54313b16861330e8a334a4d8a4
Ivydude10 <54464061+Ivydude10@users.noreply.github.com>
2019-11-18 12:05:43 +1100

    Update IBv2.py

--- IBv2.py
+++ IBv2.py
@@ -18,11 +18,32 @@ from discord.utils import get
 BOT_PREFIX = ("~", "&", "alterego ", "alter ego ", "ae ", "lvybot ")
 
 client = Bot(command_prefix=BOT_PREFIX)
+client.remove_command('help')
 
 @client.check
 async def globally_blacklist_roles(ctx):
     blacklist = ["bean of all beans", "blacklist"]  # Role names
     return not any(get(ctx.guild.roles, name=name) in ctx.author.roles for name in blacklist)
+
+@client.command()
+async def help(ctx):
+    argembed = discord.Embed(
+        colour = discord.Colour.green()
+    )
+    argembed.set_author(name="Help - ARG")
+    argembed.add_field(name='~caesar [text] [number]', value='Encodes the given string, using Caesar shift.', inline=False)
+    argembed.add_field(name='~caesarbf [text]', value='Decodes the given string, using Caesar shift bruteforce.', inline=False)
+    argembed.add_field(name='~bintext [binary data]', value='Converts the given string from binary to text. Note: Don\'t include spaces.'', inline=False)
+    argembed.add_field(name='~textbin [text]', value='Converts the given string from text to binary.', inline=False)
+    argembed.add_field(name='~htt [hex data]', value='Converts the given string from hex to text.', inline=False)
+    argembed.add_field(name='~tth [text]', value='Converts the given string from text to hex.', inline=False)
+    argembed.add_field(name='~b32tt [base32 data]', value='Converts the given string from base32 to text.', inline=False)
+    argembed.add_field(name='~ttb32 [text]', value='Converts the given string from text to base32.', inline=False)
+    argembed.add_field(name='~b64tt [base64 data]', value='Converts the given string from base64 to text.', inline=False)
+    argembed.add_field(name='~ttb64 [text]', value='Converts the given string from text to base64.', inline=False)
+
+    await ctx.send("I have dmed you my instructions.")
+    await ctx.message.author.send(embed=argembed)
 
 @client.command()
 async def load(ctx, extension):
