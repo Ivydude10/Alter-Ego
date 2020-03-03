ae8d16f30986fc7db45c960e218c31830c0bba0e
Ivydude10 <54464061+Ivydude10@users.noreply.github.com>
2020-03-03 14:18:11 +1100

    Update IBv2.py

--- IBv2.py
+++ IBv2.py
@@ -86,31 +86,6 @@ async def load(ctx, extension):
 async def unload(ctx, extension):
     client.unload_extension(f'cogs.{extension}')
 
-# @client.command()
-# async def echo(*msg):
-#     await ctx.send(' '.join(msg))
-
-#@client.event
-#async def on_message(message):
-#    if message.author == client.user:
-#        return
-#
-#    if message.author.id == 345993541643665410:
-#        await message.channel.send('Hello <@345993541643665410> o/')
-#        url = "https://discordapp.com/api/webhooks/609943520622411796/IoV84MAO7EbdVt8HAWOn1uwgAsjLM4j-YkR7NQa2lCi2V-_FoPJnde_waBN0VtKQ8mA-"
-#
-#        data = {}
-#        data["content"] = "Hello <@345993541643665410> o/"
-#        data["username"] = "Tooth Pinger 9000"
-#
-#        result = requests.post(url, data=json.dumps(data), headers={"Content-Type": "application/json"})
-#
-#        try:
-#            result.raise_for_status()
-#        except requests.exceptions.HTTPError as err:
-#            print(err)
-#        else:
-#            print("Payload delivered successfully, code {}.".format(result.status_code))
 @client.event
 async def on_ready():
     await client.change_presence(activity=Game(name="with the idea of Hope"))
